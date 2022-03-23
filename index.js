776d1f6f8b2b14969b87bad0616dc05f022333ca
Joshua Gonzalez <gonjosh@gmail.com>
2022-03-22 20:13:14 -0400

    commit tree 564554b9920a53fe35fdbd3fc126aff2d0216920

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+const newcats = ["Broom"]
+function appendCat() {
+  let newcats = ([...cats, "Broom"])
+  return newcats
+}
+const smallcats = ["Arnold"]
+function prependCat() {
+    let smallcats = (["Arnold", ...cats])
+    return smallcats
+}
+
+function removeLastCat() {
+    const tallcats = [...cats.slice(0,-1) ]
+    return tallcats
+}
+function removeFirstCat() {
+    const baldcats = [...cats.slice(-2) ]
+    return baldcats
+}
